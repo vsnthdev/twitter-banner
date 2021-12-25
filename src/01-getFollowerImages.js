1d40cc6df59d6996c0ee8bb7ccf0a7dcc49f5b21
Vasanth Developer <vsnthdev@gmail.com>
2021-12-25 21:18:22 +0530

    ✨ avatars of latest followers are now fetched

---
+++ src/01-getFollowerImages.js
@@ -0,0 +1,49 @@
+/*
+ *  Firstly fetches the latest followers and then their profile images.
+ *  Created On 25 December 2021
+ */
+
+import dotenv from 'dotenv'
+import { TwitterClient } from 'twitter-api-client'
+
+export default async env => {
+    // load the .env file
+    if (env == 'development') dotenv.config()
+
+    // grab the required variables from the environment
+    const {
+        NO_FOLLOWERS,
+        TWITTER_USERNAME,
+        TWITTER_API_KEY,
+        TWITTER_API_SECRET,
+        TWITTER_ACCESS_TOKEN,
+        TWITTER_TOKEN_SECRET,
+    } = process.env
+
+    // prepare an authenticated Twitter client
+    const twitterClient = new TwitterClient({
+        apiKey: TWITTER_API_KEY,
+        apiSecret: TWITTER_API_SECRET,
+        accessToken: TWITTER_ACCESS_TOKEN,
+        accessTokenSecret: TWITTER_TOKEN_SECRET,
+    })
+
+    // get the recent followers
+    const { users: allUsers } =
+        await twitterClient.accountsAndUsers.followersList({
+            screen_name: TWITTER_USERNAME,
+            count: Number(NO_FOLLOWERS) * 2,
+        })
+
+    return (
+        allUsers
+            // remove any protected accounts for privacy reasons
+            .filter(user => user.protected == false)
+
+            // discard the entire object and only pick the profile_image_url_https
+            .map(user => user.profile_image_url_https)
+            // field, and then replace the "_normal" with "_400x400" to get
+            // a better resolution and finally return them
+            .map(image => image.replace(/_normal/g, '_400x400'))
+    )
+}
--- src/twitter-banner.js
+++ src/twitter-banner.js
@@ -2,3 +2,16 @@
  *  Entry executable file for twitter-banner project.
  *  Created On 25 December 2021
  */
+
+import getFollowerImages from './01-getFollowerImages.js'
+
+const env = process.env.NODE_ENV || 'development'
+
+// fetch images of the latest followers from Twitter's API
+const images = await getFollowerImages(env)
+
+// 3. create a new browser instance
+// 4. inject the background image
+// 5. inject images into our document
+// 6. take a screenshot
+// 7. upload it on Twitter
